93f6db20135604f046cb084a505f0a26d91be1fb
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-23 18:52:51 -0500

    feat: add CRUD in sync

--- 03-archivos-sync.js
+++ 03-archivos-sync.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
-
-// fs.writeFileSync('./ejemplo.txt', 'Saludos tambien de Piqué!')
-
-if (fs.existsSync('./ejemplo.txt')) {
-    const contenido = fs.readFileSync('./ejemplo.txt', 'utf-8')
+const filename = './ejemplo.txt'
+fs.writeFileSync(filename, 'Saludos de Shakira!')
+if (fs.existsSync(filename)) {
+    const contenido = fs.readFileSync(filename, 'utf-8')
     console.log(contenido)
+    fs.appendFileSync(filename, '\n\tY también de Piqué')
+    fs.unlinkSync(filename, 'utf-8')
 } else {
     console.log('El archivo no existe')
 }
-
 console.log('END')
