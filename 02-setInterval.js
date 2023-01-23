656622a11fa64ce0f7a3556509929f0dfeae53e8
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-23 18:29:11 -0500

    feat: add setInterval

--- 02-setInterval.js
+++ 02-setInterval.js
@@ -1,9 +1,13 @@
-const temporizador = (cb) => {
-    setInterval(cb, 1500)
+const temporizador = () => {
+    let counter = 1
+    const timer = setInterval(() => {
+        counter++
+        console.log('ejecucion del interval')
+        if (counter > 10) clearInterval(timer)
+    }, 1500)
+
 }
 
-const operacion = () => console.log('Ejecutar cualquier cosa')
-
 console.log('Programa iniciado')
-temporizador(operacion)
+temporizador()
 console.log('Programa finalizado')
